1b6bfa6c4680d34805685133ac1a4263fd63f2af
softgoto <softgoto@gmail.com>
2015-12-21 16:10:53 +0800

    Initial Commit

---
+++ SwiftCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCounter
+//
+//  Created by xuhui on 15/12/21.
+//  Copyright © 2015年 softgoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
